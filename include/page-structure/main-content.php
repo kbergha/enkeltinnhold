e733a2b6b0185cfc6c9cbeb6e74457d554b61756
Knut Erik <kbergha@gmail.com>
2015-01-28 22:23:46 +0100

    Whopsie.

--- include/page-structure/main-content.php
+++ include/page-structure/main-content.php
@@ -1,6 +1,6 @@
 <div class="container">
 <?php
-$page = new \Enkeltinnhold\Page\Page(); // @todo: få brukt samme objekt som i startup? singelton?
+$page = new \Enkeltinnhold\Page(); // @todo: få brukt samme objekt som i startup? singelton?
 $page->resolvePage();
 
 echo $page->getPageData();
